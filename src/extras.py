924570904d770f4fb6ad162bcfcb7723468309df
klinti <businessklinti@gmail.com>
2026-09-09 16:06:56 +0200

    v0.406

--- src/extras.py
+++ src/extras.py
@@ -65,8 +65,8 @@ class LuauHighlighter(QSyntaxHighlighter):
             'mouserel', 'mousemoverel', 'getmousepos', 'getmouselocation',
             'keyclick', 'keypress', 'keyrelease', 'iswindowactive', 'isrbxactive',
             'getscriptbytecode', 'dumpstring', 'getscripthash', 'Drawing',
-            'decompile',
-            'isrenderavailable', 'getrenderproperty', 'setrenderproperty'
+            'decompile', 'saveinstance', 'savegame'
+            'isrenderavailable', 'getrenderproperty', 'setrenderproperty',
         ]
         for word in unc_keywords:
             pattern = QRegularExpression(f"\\b{word}\\b")
